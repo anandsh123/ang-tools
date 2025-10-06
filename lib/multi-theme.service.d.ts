90ef462a0a0e83ff2149d5d8732579fb26c02055
anandsh123 <anand.sh08@gmail.com>
2025-10-06 08:58:29 -0400

    Delete lib directory

--- lib/multi-theme.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MultiThemeService {
-    loadTheme(themeName: string): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<MultiThemeService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<MultiThemeService>;
-}
